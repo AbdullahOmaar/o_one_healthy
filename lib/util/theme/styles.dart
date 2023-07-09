0f4381655ed8381e2ff2cb2bdffac41d1a392664
Mohamed Atwa <matwa@ntgclarity.com>
2023-07-09 09:52:22 +0200

    Modify custom button And onpressed fun

--- lib/util/theme/styles.dart
+++ lib/util/theme/styles.dart
@@ -8,4 +8,4 @@ final tsS12W700CkPrimary = TextStyle(
 final tsS14W500CkPrimary = TextStyle(
     fontSize: 16.sp, fontWeight: FontWeight.w500, color: ThemeColors.primay);
 final blackPrimary = TextStyle(
-    fontSize: 16.sp, fontWeight: FontWeight.w500, color: ThemeColors.black);
+    fontSize: 14.sp, fontWeight: FontWeight.w500, color: ThemeColors.black);
